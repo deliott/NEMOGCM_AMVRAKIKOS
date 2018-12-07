35c3f28c488397db164d4e5ba3247c93dcf13723
deliott <eliottdupont@laposte.net>
2018-12-07 14:29:56 +0100

    changes to main to deal with separation of plot and writing

--- matlab/ecmwf/main.m
+++ matlab/ecmwf/main.m
@@ -1,11 +1,12 @@
-% This matlab script intend to shape the ecmwf raw data to feed NEMO
+% This matlab script shapes the ecmwf raw data to feed NEMO
 % 
 % The output file are ecmwf_y1987m**.nc'
 % 
-
 % 
 % It uses the functions  fill_nan.m   convert_accu.m   and interpol.m  
 % 
+% It can call plot_data.m and write_ecmwf.m
+%
 % Modified by Eliott the 9th of July 2018
 %
 
@@ -13,18 +14,24 @@
 clear;
 close all;
 
+plot_of_data = false; 
+writing_of_data = false; 
+deal_with_all_files = false ; %false -> for test with only one file
+
 %% load data fron coarse ncdf
 % Read input_file
-% filename_input = '/home/lifewatch-user/working_directory/ecmwf_shape_input_0607/withMATLAB/test.nc';
-% filename_output = '/home/lifewatch-user/working_directory/ecmwf_shape_input_0607/withMATLAB/test_y2016m12.nc' 
 
 % file_list = '/home/lifewatch-user/Downloads/PFE/ecmwf_1987_input/input/1987_*.nc' 
 
-input_dirName = '/home/lifewatch-user/Downloads/PFE/ecmwf_1987_input/input/';  
-output_dirName = '/home/lifewatch-user/Downloads/PFE/ecmwf_1987_input/output/';  
+input_dirName  = '/home/eliott/PFE/GITHUB/NEMOGCM_AMVRAKIKOS/matlab/ecmwf/input/';  
+output_dirName = '/home/eliott/PFE/GITHUB/NEMOGCM_AMVRAKIKOS/matlab/ecmwf/output/';  
 
-files = dir( fullfile(input_dirName,'*.nc') );   %# list all *.xyz files
-files = {files.name}';                      %'# file names
+if deal_with_all_files
+    files = dir( fullfile(input_dirName,'*.nc') );   % list all *.xyz files
+else 
+    files = dir( fullfile(input_dirName,'y1987m01.nc') ); % uses only one file as input
+end
+    files = {files.name}';                      % file names
 
 
 %% Work to be done outside the for loop 
@@ -54,16 +61,16 @@ for i=1:numel(files)
     filename_output = fullfile(output_dirName, files{i}); 
     
         %% reading the input data
-    lon   = ncread(filename_input, 'longitude');
-    lat   = ncread(filename_input, 'latitude');
-    time  = ncread(filename_input, 'time');
-    d2m = ncread(filename_input, 'd2m');
-    msl = ncread(filename_input, 'msl');
-    t2m = ncread(filename_input, 't2m');
-    tcc = ncread(filename_input, 'tcc');
-    tp = ncread(filename_input, 'tp');
-    u10 = ncread(filename_input, 'u10');
-    v10 = ncread(filename_input, 'v10'); 
+    lon  = ncread(filename_input, 'longitude');
+    lat  = ncread(filename_input, 'latitude');
+    time = ncread(filename_input, 'time');
+    d2m  = ncread(filename_input, 'd2m');
+    msl  = ncread(filename_input, 'msl');
+    t2m  = ncread(filename_input, 't2m');
+    tcc  = ncread(filename_input, 'tcc');
+    tp   = ncread(filename_input, 'tp');
+    u10  = ncread(filename_input, 'u10');
+    v10  = ncread(filename_input, 'v10'); 
         %% Fill missing values
     lon(isnan(lon)) = 1.000000020040877e+20 ;
     lat(isnan(lat)) = 1.000000020040877e+20 ;
@@ -73,12 +80,12 @@ for i=1:numel(files)
     msl = fill_nan(msl);
     t2m = fill_nan(t2m);
     tcc = fill_nan(tcc);
-    tp = fill_nan(tp);
+    tp  = fill_nan(tp);
     u10 = fill_nan(u10);
     v10 = fill_nan(v10);
         %% Convert accumulated data over several time step to accumulation over a single timestep
     tp_notConverted = tp;
-    tp = convert_accu(tp);
+    tp  = convert_accu(tp);
  
     
 
@@ -122,44 +129,7 @@ t2m_m = [];
 
     
 end
-t1 = datetime(1987,01,1,0,0,0);
-t2 = datetime(1988,01,31,23,59,59);
-t = t1:t2;
 
-subplot(3,1,1);
-
-plot( t, T2M-273.15)
-title('Air Temperature (C)')
-xlim([t1 t2])
-ylim([-5 35])
-    hline = ones(1,size(t,2))*0;%mean(T2M-273.15);
-hold on
-plot( t , hline)
-hold off
-
-subplot(3,1,2);
-
-plot( t, U10)
-title('Wind velocity, x-component (m/s)')
-xlim([t1 t2])
-ylim([-6 6])
-    hline = ones(1,size(t,2))*mean(U10);
-hold on
-plot( t , hline)
-hold off
-
-subplot(3,1,3);
-
-
-plot( t, V10)
-title('Wind velocity, y-component (m/s)')
-xlim([t1 t2])
-ylim([-6 6])
-    hline = ones(1,size(t,2))*mean(V10);
-hold on
-plot( t , hline)
-hold off
-
-% sgtitle('My Subplot Grid Title')
-
-
+if plot_of_data 
+    plot_data
+end
