ce439426d988bbd95f4941a97010dceea7775242
deliott <eliottdupont@laposte.net>
2018-12-13 21:24:43 +0100

    improved convert_accu documentation

--- matlab/ecmwf/convert_accu.m
+++ matlab/ecmwf/convert_accu.m
@@ -1,17 +1,30 @@
 function [accu_on_time_step] = convert_accu( field )
+%
+% This function deals with accumulation data from ecmwf. (total precipitation)
+% 
+% Convert accumulated data over several time step to accumulation over a single timestep
+%INPUT :
+%      field  = timeserie of ecmwf raw data after fill_nan was applied, thrid dimension is time.
+%OUTPUT : 
+%      accu_on_time_step = time series of fields with instantaneous data instead of accumulated data.
+%
+%    uses the built-in function interp2
+%
+% The following ressources were used to understand the data structure : 
 % https://software.ecmwf.int/wiki/pages/viewpage.action?pageId=56658233
-% for how to deal with accumulated data with forecast type of interim data
-% for time conversion into meter per 3 hours
-% for other units convertions : https://www.researchgate.net/post/How_do_I_convert_ERA_Interim_precipitation_estimates_from_kg_m2_s_to_mm_day
-% 86400 mm/day = 1 kg.m-2.s-1
-% 8*n/86.4 -> convert meters per 3h to kg.m-2.s-1
+% 	for how to deal with accumulated data with forecast type of interim data
+%	for time conversion into meter per 3 hours
+% https://www.researchgate.net/post/How_do_I_convert_ERA_Interim_precipitation_estimates_from_kg_m2_s_to_mm_day
+% 	for other units convertions :
+%		86400 mm/day = 1 kg.m-2.s-1
+% 		8*n/86.4 -> convert meters per 3h to kg.m-2.s-1
 
 accu_on_time_step = field;
 time_size= size(field(1,1,:),3);
 
 for time_step = 1:time_size 
     %     if mod(time_step,4) == 1
-%         accu_on_time_step(:,:,time_step) = field(:,:,time_step)* (8/86.4)  ; % from 00:00 to 03:00 and from from 12:00 to 15:00 no calculation required
+    %         accu_on_time_step(:,:,time_step) = field(:,:,time_step)* (8/86.4)  ; % from 00:00 to 03:00 and from from 12:00 to 15:00 no calculation required
     %     else
     % % %    DOESN'T LOOK NECESSARY AFTER ALL (GIVES NEGATIVE VALUES ...)    accu_on_time_step(:,:,time_step) = field(:,:,time_step)  - field(:,:,time_step -1) ;  %% We now have meters per 3 hours. 
     %         accu_on_time_step(:,:,time_step) = accu_on_time_step(:,:,time_step) * (8/86.4);  %% we now have kg.m-2.s-1
